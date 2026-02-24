404d57c57021b6381663b7f6d3b4612751aa41e8
joeyscags <joeyscags5000@gmail.com>
2026-02-24 20:05:35 +0100

    implement swap, min, and max functions in whatever.hpp

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -14,12 +14,23 @@
 # define WHATEVER_HPP
 
 template <typename T>
-void	swap( T &a, T &b );
+void	swap( T &a, T &b )
+{
+	T	tmp = a;
+	a = b;
+	b = tmp;
+}
 
 template <typename T>
-T	min( T a, T b );
+T	min( T a, T b )
+{
+	return (b < a) ? b : a;
+}
 
 template <typename T>
-T	max( T a, T b );
+T	max( T a, T b )
+{
+	return (b > a) ? b : a;
+}
 
 #endif
