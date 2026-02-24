323caa784df1800be5c52b55374383a893c6f63b
joeyscags <joeyscags5000@gmail.com>
2026-02-25 00:32:09 +0100

    update main.cpp to demonstrate swap, min, and max functions; update header comment in whatever.hpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 16:38:57 by joeyscags         #+#    #+#             */
-/*   Updated: 2026/02/24 16:38:58 by joeyscags        ###   ########.fr       */
+/*   Updated: 2026/02/25 00:31:39 by joeyscags        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,21 @@
 
 int main( void )
 {
+	int	a = 2;
+	int	b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string	c = "chaine1";
+	std::string	d = "chaine2";
+
+	::swap( c, d );
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
 	return 0;
 }
