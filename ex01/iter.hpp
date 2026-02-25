de125b3b7b0de067033e881c9730b6ecde1ecbbd
joeyscags <joeyscags5000@gmail.com>
2026-02-25 14:49:35 +0100

    implement iter function templates for array iteration

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,4 +1,20 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <cstddef>
+
+template <typename T, typename F>
+void	iter( T *arr, const std::size_t len, F func )
+{
+	for (std::size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
+
+template <typename T, typename F>
+void	iter( const T *arr, const std::size_t len, F func )
+{
+	for (std::size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
+
 #endif
