c94cf6d20799cde273e1789f3f32f25909dcda4b
joeyscags <joeyscags5000@gmail.com>
2026-02-25 17:08:33 +0100

    add array iteration examples in main.cpp with print and increment functions

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,7 +2,36 @@
 #include <string>
 #include "iter.hpp"
 
+template <typename T>
+void	print( T const &x )
+{
+	std::cout << x << std::endl;
+}
+
+template <typename T>
+void	increment( T &x )
+{
+	x++;
+}
+
 int main( void )
 {
+	int			intArr[] = {1, 2, 3, 4, 5};
+	std::string	strArr[] = {"hello", "world", "42"};
+	const int	constArr[] = {10, 20, 30};
+
+	std::cout << "--- int array ---" << std::endl;
+	::iter(intArr, 5, print<int>);
+
+	std::cout << "--- increment int array ---" << std::endl;
+	::iter(intArr, 5, increment<int>);
+	::iter(intArr, 5, print<int>);
+
+	std::cout << "--- string array ---" << std::endl;
+	::iter(strArr, 3, print<std::string>);
+
+	std::cout << "--- const int array ---" << std::endl;
+	::iter(constArr, 3, print<int>);
+
 	return 0;
 }
